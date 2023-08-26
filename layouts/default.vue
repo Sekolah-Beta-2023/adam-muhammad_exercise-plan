c59265895a528fb09670684401e0f1a783cd601a
adammmmd <adam.muhammad.onduty@gmail.com>
2023-08-26 19:23:38 +0700

    add navbar, footer

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <Header />
+
+        <Nuxt />
+
+        <Footer />
+    </div>
+</template>
+
+<script>
+// memanggil component sesuai lokasi masing masing file
+import Header from "@/components/Header.vue"
+import Footer from "@/components/Footer.vue"
+
+export default {
+    // Dideklarasikan agar dapat diakses oleh template
+    components: {
+        Header,
+        Footer
+    }
+}
+</script>
+
+<style>
+</style>
